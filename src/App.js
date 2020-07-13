a348fc88a235b0f974329bc39ac8440642775d13
Hoang Vu <hoang.vu@uafs.edu>
2020-07-13 16:13:26 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  postHandle = () => {
+    axios
+      .post("https://fir-app-5824c.firebaseio.com/person.json", {
+        body: {
+          name: "Tom",
+          age: 40,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getHandle = () => {
+    axios
+      .get("https://fir-app-5824c.firebaseio.com/person.json")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  updateHandle = () => {
+    const id = "-MBpkTJeJl-JBD7T30Mf";
+
+    axios
+      .put(`https://fir-app-5824c.firebaseio.com/person/${id}.json`, {
+        body: {
+          name: "sarah",
+          age: 12,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  deleteHandle = () => {
+    const id = "-MBpkTJeJl-JBD7T30Mf";
+
+    axios
+      .delete(`https://fir-app-5824c.firebaseio.com/person/${id}.json`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>Hello</div>
+        <button onClick={this.postHandle}>POST</button>
+        <button onClick={this.getHandle}>GET</button>
+        <button onClick={this.updateHandle}>UPDATE</button>
+        <button onClick={this.deleteHandle}>DELETE</button>
+      </div>
+    );
+  }
 }
 
 export default App;
